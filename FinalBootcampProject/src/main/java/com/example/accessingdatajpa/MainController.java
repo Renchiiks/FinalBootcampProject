607b137d75622e04cc35c5869b7a67f97d980bef
KristinaG21 <kristina.kondratjeva@gmail.com>
2020-05-28 00:39:55 +0300

    wep pages connection

--- FinalBootcampProject/src/main/java/com/example/accessingdatajpa/MainController.java
+++ FinalBootcampProject/src/main/java/com/example/accessingdatajpa/MainController.java
@@ -4,26 +4,39 @@ import com.example.accessingdatajpa.data.TourismObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
-@RequestMapping(path = "/api")
-
 public class MainController {
+
+
     @Autowired
     private TourismObjectService service;
 
 
-    @GetMapping(path = "/all")
-    public @ResponseBody
-    List<TourismObject> getAllObjects() {
-        return service.findAll();
+    @GetMapping("/citytourism")
+    public String cityTourism(Model model){
+        List<TourismObject> getAllObjects=service.findAll();
+        model.addAttribute("getAllObjects", getAllObjects);
+        return "citytourism";
+
+    }
+    @GetMapping("/countrytourism")
+    public String countryTourism(Model model){
+        List<TourismObject> getAllObjects=service.findAll();
+        model.addAttribute("getAllObjects", getAllObjects);
+        return "countrytourism";
+
     }
 
 
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Main Page");
+        return "home";
+    }
 }
 
